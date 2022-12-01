030a8a763aa5350f73b8db54ccd6b58d8914411b
Toty-01 <caucheanthony@gmail.com>
2022-12-01 12:46:04 +0100

    eval a valider

--- cinema.sql
+++ cinema.sql
@@ -1,39 +1,33 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
 
---
+
 -- Base de données : `cinema`
---
-CREATE DATABASE IF NOT EXISTS 'cinema' SET utf8mb4 COLLATE utf8mb4_general_ci;
--- --------------------------------------------------------
 
---
+
+CREATE DATABASE IF NOT EXISTS 'cinema' SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+
 -- Structure de la table `admin`
---
+
 
 CREATE TABLE `admin` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `id` PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `nom_admin` varchar(50) NOT NULL,
   `prénom_admin` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `permission` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 INSERT INTO `admin` (`id`, `nom_admin`, `prénom_admin`, `password`, `permission`) VALUES
-(4, 'verk', 'thierry', '$2y$10$uNHmehOPvWqRRUg3TCB.w.xcNDK39PdjbAHS/ZWT2TNHCFD2cPXri', 'admin'),
-(5, 'pilou', 'philou', '$2y$10$XataTzHRxM5rvqzgnFUMoObiW7iDHDrXQ3f.m7iRuzCu1vkBAFFbm', 'editeur'),
-(6, 'gerdanzo', 'jojo', '$2y$10$jv9ApS/HaIT5VfZWmf7y0Oq1FDN0tjH3WcNAaBFAcn230MJthUi7K', 'admin'),
-(8, 'charles', 'John', '$2y$10$26Hf0GaTUhnuHVf6mkM5sOfvyW09UHfL/lhYOqsS3rqAwcMIDEfju', 'editeur'),
-(9, 'priter', 'franck', '$2y$10$iFR.f/2AZpZE00/zAa54nOAjIdaJ0BzpSEZt7YjdNPzdImJrQGfSC', 'editeur'),
-(10, 'jarry', 'Steeve', '$2y$10$DZSsyLXqnZcRL7shGbWnm.i2YwAcbwEkX5FJe38b9So', 'admin');
+(1, 'verk', 'thierry', '$2y$10$uNHmehOPvWqRRUg3TCB.w.xcNDK39PdjbAHS/ZWT2TNHCFD2cPXri', 'admin'),
+(2, 'gerdanzo', 'jojo', '$2y$10$jv9ApS/HaIT5VfZWmf7y0Oq1FDN0tjH3WcNAaBFAcn230MJthUi7K', 'admin'),
+(3, 'jarry', 'Steeve', '$2y$10$DZSsyLXqnZcRL7shGbWnm.i2YwAcbwEkX5FJe38b9So', 'admin');
+(4, 'pibourse', 'steevy', '$2y$10$b3qEhQc172Nf2F2IHYZfz.t1W71Ps4y4jEEzPAmxjkfCwX5fPSgca', 'admin'),
 
- --------------------------------------------------------
 
---
 -- Structure de la table `cinema`
---
+
 
 CREATE TABLE `cinema` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -44,21 +38,18 @@ CREATE TABLE `cinema` (
   `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 INSERT INTO `cinema` (`id`, `nom`, `adresse`, `ville`, `telephone`, `email`) VALUES
 (1, 'pathé', '15 avenue champagne', 'dijon', '0622443377', 'ks.ieieki@lzoz.net'),
 (2, "carré de l\'autre", '18 rue des prés', 'mâcon', '0122445577', 'ks.ivvvv@lzoz.net'),
 (3, 'les toiles', '15 hameau des vaches', 'montpellier', '0444664747', 'ks.ieiddfi@lzoz.net'),
 (4, 'les projo', '12 rue des halles', 'paris', '0411445566', 'ks.ievdd@lzoz.net'),
 (5, 'cinéstéthique', '6 avenue des étoiles', 'lyon', '0344558844', 'ks.ieieki@lzoz.net'),
-(10, 'cinépolice', '52 avenue des champs élysées', 'lille', '0122114477', 'ks.ievvx@lzoz.net'),
-(12, 'ciné phil', '34 rue des fourneaux', 'marseille', '0122445566', 'ks.imoli@lzoz.net');
+(6, 'cinépolice', '52 avenue des champs élysées', 'lille', '0122114477', 'ks.ievvx@lzoz.net'),
+(7, 'ciné phil', '34 rue des fourneaux', 'marseille', '0122445566', 'ks.imoli@lzoz.net');
 
--- --------------------------------------------------------
 
---
 -- Structure de la table `client`
---
+
 
 CREATE TABLE `client` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -71,7 +62,6 @@ CREATE TABLE `client` (
   `password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 INSERT INTO `client` (`id`, `nom`, `prénom`, `adresse`, `ville`, `téléphone`, `date de naissance`, `email`, `password`) VALUES
 (1, 'perouve', 'guy', '61 grande rue', 'paris', '0452663322', '1989-10-20', 'vinc.jdj@peoep.com', '$2y$10$IeQL99ItCqhlWzHCqn8hgeOcnGusR2eeXXvdL81YvMuxW5Eqr8oCG'),
 (2, 'grizoou', 'esteban', '45 rue des pubilles', 'lyon', '0452663322', '2001-08-08', 'lfofpfkf@jdjdj.pon', '$2y$10$rKMFa3rK1kVm4qNRNh6w0.F5iUXra05nHprnjj0tuye4oWzDeg9RW'),
@@ -83,20 +73,17 @@ INSERT INTO `client` (`id`, `nom`, `prénom`, `adresse`, `ville`, `téléphone`,
 (8, 'pibourse', 'steevy', '6 avenue des tilleuls', 'lyon', '0955664477', '1989-05-05', 'jfjfjf.kdk@pdlldl.com', '$2y$10$b3qEhQc172Nf2F2IHYZfz.t1W71Ps4y4jEEzPAmxjkfCwX5fPSgca'),
 (9, 'thuram', 'pilou', '55 rue des sables', 'mâcon', '0244558899', '1999-04-01', 'ooddpd@palapz.com', '$2y$10$Z3jUDR.RSA6MwCEEWxecM.WXrzF1dgkIiODk3UwkxUu2gxp7DMSq');
 
--- --------------------------------------------------------
 
---
 -- Structure de la table `film`
---
+
 
 CREATE TABLE `film` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `nom` varchar(250) NOT NULL,
   `durée` int(11) NOT NULL,
-  ADD FOREIGN KEY (cinema ID)
-  REFERENCES cinema (cinema ID)
+  FOREIGN KEY (cinema_id)
+  REFERENCES cinema (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO `film` (`id`, `nom`, `durée`) VALUES
 (1, 'blink', 182),
@@ -106,19 +93,16 @@ INSERT INTO `film` (`id`, `nom`, `durée`) VALUES
 (5, 'le dernier des cahimans', 102),
 (6, 'very good trip', 86);
 
--- --------------------------------------------------------
 
---
 -- Structure de la table `genre`
---
+
 
 CREATE TABLE `genre` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `type` varchar(250) NOT NULL,
-  ADD FOREIGN KEY (film ID)
-  REFERENCES film (film ID)
+  ADD FOREIGN KEY (film_id)
+  REFERENCES film (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO `genre` (`id`, `type`) VALUES
 (1, 'horreur, comédie'),
@@ -129,19 +113,16 @@ INSERT INTO `genre` (`id`, `type`) VALUES
 (6, 'fiction, aventure'),
 (7, 'horreur, comédie');
 
----------------------------------------------------
 
---
 -- Structure de la table `réservation`
---
+
 
 CREATE TABLE `réservation` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `horaire` datetime NOT NULL,
-  `tarif` int(11) NOT NULL
+  `tarif` DECIMAL(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO `réservation` (`id`, `date`, `horaire`, `tarif`) VALUES
 (1, '2023-01-19', '15:00:00', 9.20),
@@ -152,17 +133,14 @@ INSERT INTO `réservation` (`id`, `date`, `horaire`, `tarif`) VALUES
 (6, '2023-01-20', '16:00:00', 7.60),
 (7, '2023-01-20', '09:00:00', 5.90);
 
--- --------------------------------------------------------
 
---
 -- Structure de la table `salle`
---
+
 
 CREATE TABLE `salle` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `capacity` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO `salle` (`id`, `capacity`) VALUES
 (1, 120),
@@ -178,21 +156,18 @@ INSERT INTO `salle` (`id`, `capacity`) VALUES
 (11, 415),
 (12, 420);
 
--- --------------------------------------------------------
 
---
 -- Structure de la table `séance`
---
+
 
 CREATE TABLE `séance` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
   `heure` time NOT NULL,
   `film` varchar(250) NOT NULL,
-  ADD FOREIGN KEY (film ID)
-  REFERENCES film (film ID)
+  FOREIGN KEY (film_id)
+  REFERENCES film (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO `séance` (`id`, `date`, `heure`, `film`) VALUES
 (1, '2023-03-15', '15:00:00', 'groobs'),
@@ -204,11 +179,9 @@ INSERT INTO `séance` (`id`, `date`, `heure`, `film`) VALUES
 (7, '2023-03-18', '14:15:00', 'be bol'),
 (8, '2023-03-15', '18:20:00', 'avatar 5');
 
--- --------------------------------------------------------
 
---
 -- Structure de la table `user`
---
+
 
 CREATE TABLE `user` (
   `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -216,7 +189,6 @@ CREATE TABLE `user` (
   `prénom` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO `user` (`id`, `nom`, `prénom`, `password`) VALUES
 (1, 'shpoung', 'valentin', '$2y$10$kmJMj0a4G1r.h6EcSwHuGu3rO.YgCLFJqit0.eq9s7n'),
